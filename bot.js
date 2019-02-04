99c368b6bcf1d2586648de8a5f8e982e559e119e
ahmet889 <47326815+ahmet889@users.noreply.github.com>
2019-02-04 18:15:30 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,50 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`[BOT] ${client.user.tag} ADI İLE GİRİŞ YAPTI!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'lag var') {
+    msg.reply('f8 filtera hemen göz atıyoruz.');
+  }
+  
+   if (msg.content === 'sw gitti') {
+    msg.reply('hemen yetkili arkadaşlarıma bildirim yolluyorum.');
+  }
+  
+   if (msg.content === 'sw gg') {
+    msg.reply('Komple kapanma sorunu yoktur. Hemen Prosedur Yetkilisine bildiriyorum.');
+  }
+  
+   if (msg.content === 'Selamın Aleyküm') {
+    msg.reply('Aleyküm Selam. Hoşgeldin iyi oyunlar.');
+  }
+  
+   if (msg.content === 'sa') {
+    msg.reply('Aleyküm Selam. Hoşgeldin iyi oyunlar.');
+  }
+  
+   if (msg.content === 's.a') {
+    msg.reply('Aleyküm Selam. Hoşgeldin iyi oyunlar.');
+  }
+  
+   if (msg.content === 'lag var') {
+    msg.reply('f8 filtera hemen göz atıyoruz.');
+  }
+  
+  if (msg.content === 'bi yetkili yazabilir mi') {
+    msg.reply('Hygge Size geri dönüş sağlaması için bildirim yolladım.');
+  }
+  
+  if (msg.content === 'GM bakarmısın') {
+    msg.reply('Hygge Adlı Yetkiliye Mesaj yazabilirsiniz.');
+  }
+  
+  if (msg.content === 'selam') {
+    msg.reply('Selam :) İYİ OYUNLAR');
+  }
+});
+
+client.login('NTQxMzExOTI5MzkwNDY1MDM4.DzijmQ.cOKkc4o7-LUOlzTbaiA-frGW_s8');
